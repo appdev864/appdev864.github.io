72f74c21d6ef2aad5830640902b1b87fb48cb53c
appdev864 <appdev864@gmail.com>
2021-07-07 22:10:39 -0400

    Hello world typewriter

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -10,23 +10,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
           <Typewriter
             options={{
-            strings: ['Hello', 'World'],
+            strings: ['Hello World'],
             autoStart: true,
             loop: true,
           }}
         />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
